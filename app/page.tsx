3663b0d8fe5451ee590768839d3079a1347acc91
Mikael <mikaelmello.p@gmail.com>
2024-01-24 12:54:35 -0800

    Workinginprogress

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-start p-4 md:p-24 lg:p-48 xl:p-64">
       <div>
         <h1 className="p-4">Mikael Mello</h1>
-        <p className="p-4 font-light">Crafting interfaces. Building polished software and web experiences. Experimenting with magical details in user interfaces. Webmaster at Linear.</p>
-        <p className="p-4 font-light">In the past I’ve developed the Vercel design system, website, and dashboard.</p>
+        <p className="p-4 font-light">This is a working in progress. I am a work in progress.</p>
+        <p className="p-4 font-light">You'll be able to see here, soon, a little preview of the work and progress i have been working on.</p>
       </div>
 
-      <div className="w-full px-4 md:px-8">
+      <div className="w-full px-4 md: px-24">
         <CodeBlock
           quote="“Dwell on the beauty of life. Watch the stars, and see yourself running with them.”"
           author="― Marcus Aurelius"
